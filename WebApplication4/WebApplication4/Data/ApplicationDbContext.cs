4e9b723b76b2731dbcbf8ff5fb817a5fe3caaebd
jaydeepsingh1104 <65841968+jaydeepsingh1104@users.noreply.github.com>
2023-04-04 16:21:54 +0530

    inint commit

---
+++ WebApplication4/WebApplication4/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
